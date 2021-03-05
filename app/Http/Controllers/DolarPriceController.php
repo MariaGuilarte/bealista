14f60e94a837eee761a7842c4d73193d1ad5e4ca
N3r1 <mariajoseguilarte@gmail.com>
2021-03-05 18:44:04 -0400

    Initial commit

---
+++ app/Http/Controllers/DolarPriceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DolarPrice;
+use Illuminate\Http\Request;
+
+class DolarPriceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DolarPrice  $dolarPrice
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DolarPrice $dolarPrice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DolarPrice  $dolarPrice
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DolarPrice $dolarPrice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\DolarPrice  $dolarPrice
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DolarPrice $dolarPrice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DolarPrice  $dolarPrice
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DolarPrice $dolarPrice)
+    {
+        //
+    }
+}
